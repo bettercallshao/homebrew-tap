6cf783cf64e1da07204e68023752388abdabecb9
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-17 16:41:05 -0400

    Brew formula update for kwt version v0.5.8

--- Formula/kwt.rb
+++ Formula/kwt.rb
@@ -5,27 +5,28 @@
 class Kwt < Formula
   desc "Run commands easily"
   homepage "https://github.com/bettercallshao/kwt"
-  version "0.5.7"
+  version "0.5.8"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/bettercallshao/kwt/releases/download/v0.5.7/kwt_0.5.7_darwin_amd64.zip"
-    sha256 "1383130e9037e020dda304e52aaebae16aac88b20e8bb165e81cdd7ba7ca1b45"
+    url "https://github.com/bettercallshao/kwt/releases/download/v0.5.8/kwt_0.5.8_darwin_amd64.zip"
+    sha256 "3f31b521536494c3c7a9b4121ec8b11ae399cc39ea1f55bc623936bbd4d1d481"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/bettercallshao/kwt/releases/download/v0.5.7/kwt_0.5.7_linux_amd64.zip"
-    sha256 "b53a990065e03ef83c0987e63042127be9e659fd74e391ee5e5c108c5df6dd2e"
+    url "https://github.com/bettercallshao/kwt/releases/download/v0.5.8/kwt_0.5.8_linux_amd64.zip"
+    sha256 "c6752ebd839f7684011c2fe4c3c6b59ea22664c240cb4c77de09badec41f0f1a"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/bettercallshao/kwt/releases/download/v0.5.7/kwt_0.5.7_linux_armv6.zip"
-    sha256 "667a8dbda2a73ec132b0d7ddea342e6b7539c9d1c5e8c3f0de311b609c2315e0"
+    url "https://github.com/bettercallshao/kwt/releases/download/v0.5.8/kwt_0.5.8_linux_armv6.zip"
+    sha256 "b273e43ff58afb743a872b6922596978693907f96ccc82dadeaaa56a6f5d603e"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/bettercallshao/kwt/releases/download/v0.5.7/kwt_0.5.7_linux_arm64.zip"
-    sha256 "963f8edf7524f6e60a57aa56cd9968134aed8fceb300d99a007373b6ae243891"
+    url "https://github.com/bettercallshao/kwt/releases/download/v0.5.8/kwt_0.5.8_linux_arm64.zip"
+    sha256 "3e07d9244716c6ea409dfd4be8d7219dfbdf9da3a0e8f1dc286a6f1faef814e9"
   end
 
   def install
     bin.install "kwt"
+    bin.install "kwtd"
   end
 end
